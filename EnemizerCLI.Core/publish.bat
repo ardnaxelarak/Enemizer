68d201fceee1f6ba3d5c9a76f7e2ade0027cba65
Hussein Farran <hmfarran@gmail.com>
2020-06-10 16:33:43 -0400

    Add workaround for incompatibility between msbump and SDK 3.1.

--- EnemizerCLI.Core/publish.bat
+++ EnemizerCLI.Core/publish.bat
@@ -1,3 +1,7 @@
-dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime win-x64
-dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime osx.10.12-x64
-dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime ubuntu.16.04-x64
+@echo off
+setlocal
+  set MSBUILDSINGLELOADCONTEXT=1
+  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime win-x64
+  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime osx.10.12-x64
+  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime ubuntu.16.04-x64
+endlocal
