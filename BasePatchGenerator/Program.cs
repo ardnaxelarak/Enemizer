296eee73131515bf1fddedb49ac6c5d419995d3f
caitsith2 <d_good@caitsith2.com>
2020-07-21 20:17:48 -0700

    Changes: * Use assembly prebuild to delete all instances of patchData.json, exported_symbols.txt and enemizerBasePatch.json * Use assembly post-build to assemble the assembly into patchData.json and exported_symbols.txt * Move BasePatchGenerator stuff from Enemizer prebuild to BasePatchGenerator post build * BasePatchGenerator no longer using hard-coded names. those are now supplied by the postbuild script. * BasePatchGenerator now verifies that the rom is correct before applying the patchData. * stop copying patchData.json, exported_symbols.txt and enemizerBasePatch.json to directories where it is not needed.

--- BasePatchGenerator/Program.cs
+++ BasePatchGenerator/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mime;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,29 +12,54 @@ namespace BasePatchGenerator
 {
     class Program
     {
-        static void Main(string[] args)
+	    private static void Main(string[] args)
         {
-            FileStream fs = new FileStream("zelda.sfc", FileMode.Open, FileAccess.Read);
-            byte[] rom_data = new byte[fs.Length];
+	        if (args.Length != 3)
+	        {
+				Console.WriteLine("Usage: BasePatchGenerator.exe [rom file] [patch data] [output file]");
+				return;
+	        }
+
+	        Console.WriteLine($"Reading rom file {args[0]}");
+            FileStream fs = new FileStream(args[0], FileMode.Open, FileAccess.Read);
+            var rom_data = new byte[fs.Length];
             fs.Read(rom_data, 0, (int)fs.Length);
             fs.Close();
 
+            if (rom_data.Length % 1024 == 512)
+            {
+	            Console.WriteLine("Stripping rom header");
+	            rom_data = rom_data.Skip(512).ToArray();
+	        }
+
+            using (MD5 md5 = MD5.Create())
+            {
+				Console.WriteLine("Verifying rom");
+	            byte[] hash = md5.ComputeHash(rom_data);
+	            if (!hash.SequenceEqual(new byte[] {0x03, 0xa6, 0x39, 0x45, 0x39, 0x81, 0x91, 0x33, 0x7e, 0x89, 0x6e, 0x57, 0x71, 0xf7, 0x71, 0x73}))
+	            {
+					throw new Exception("Invalid rom file");
+	            }
+            }
+
+			Console.WriteLine("Applying Patch to rom");
             Array.Resize(ref rom_data, 4 * 1024 * 1024);
 
             RomData rom = new RomData(rom_data);
 
-            Patch patch = new Patch("patchData.json");
+            Patch patch = new Patch(args[1]);
             patch.PatchRom(rom);
 
             GeneralPatches.MoveRoomHeaders(rom);
 
-            var patches = rom.GeneratePatch();
+            List<PatchObject> patches = rom.GeneratePatch();
 
             patch.AddPatches(patches);
 
             var patchJson = patch.ExportJson();
-
-            File.WriteAllText("enemizerBasePatch.json", patchJson);
+			
+			Console.WriteLine($"Writing output file {args[2]}");
+			File.WriteAllText(args[2], patchJson);
         }
     }
 }
