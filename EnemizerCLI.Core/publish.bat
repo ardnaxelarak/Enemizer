21910467cc86bcb828eac7668db258eb578fd418
Hussein Farran <hmfarran@gmail.com>
2020-07-27 20:00:48 -0400

    Update release script as well as version uptick.

--- EnemizerCLI.Core/publish.bat
+++ EnemizerCLI.Core/publish.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
   set MSBUILDSINGLELOADCONTEXT=1
-  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime win-x64
-  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime osx.10.12-x64
-  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime ubuntu.16.04-x64
+  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime win-x64 -p:PublishTrimmed=true
+  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime osx.10.12-x64 -p:PublishTrimmed=true
+  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime ubuntu.16.04-x64 -p:PublishTrimmed=true
 endlocal
