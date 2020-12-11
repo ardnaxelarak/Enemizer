a6b34655618d0a5bb68537f072c8585f30c2a810
CaitSith2 <d_good@caitsith2.com>
2020-12-11 12:10:05 -0800

    Revise publishing scripts

--- EnemizerCLI.Core/publish.bat
+++ EnemizerCLI.Core/publish.bat
@@ -1,7 +1,25 @@
 @echo off
 setlocal
+  PATH="C:\Program Files\7-Zip";%PATH%
   set MSBUILDSINGLELOADCONTEXT=1
-  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime win-x64 -p:PublishTrimmed=true
-  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime osx.10.12-x64 -p:PublishTrimmed=true
-  dotnet publish --framework netcoreapp2.0 -c Release --self-contained --runtime ubuntu.16.04-x64 -p:PublishTrimmed=true
+  
+  set FRAMEWORK=netcoreapp3.1
+  set RELEASEBUILD=Release
+  
+  set WINDOWSVERSION=win-x64
+  set MACVERSION=osx.10.12-x64
+  set LINUXVERSION=ubuntu.16.04-x64
+  
+  dotnet publish --framework %FRAMEWORK% -c %RELEASEBUILD% --self-contained --runtime %WINDOWSVERSION% -p:PublishTrimmed=true
+  dotnet publish --framework %FRAMEWORK% -c %RELEASEBUILD% --self-contained --runtime %MACVERSION% -p:PublishTrimmed=true
+  dotnet publish --framework %FRAMEWORK% -c %RELEASEBUILD% --self-contained --runtime %LINUXVERSION% -p:PublishTrimmed=true
+  
+  7z a .\bin\%RELEASEBUILD%\%WINDOWSVERSION%.7z .\bin\%RELEASEBUILD%\%FRAMEWORK%\%WINDOWSVERSION%\publish\*
+  7z a .\bin\%RELEASEBUILD%\%MACVERSION%.7z .\bin\%RELEASEBUILD%\%FRAMEWORK%\%MACVERSION%\publish\*
+  7z a .\bin\%RELEASEBUILD%\%LINUXVERSION%.7z .\bin\%RELEASEBUILD%\%FRAMEWORK%\%LINUXVERSION%\publish\*
+  7z a -tzip .\bin\%RELEASEBUILD%\%WINDOWSVERSION%.zip .\bin\%RELEASEBUILD%\%FRAMEWORK%\%WINDOWSVERSION%\publish\*
+  7z a -tzip .\bin\%RELEASEBUILD%\%MACVERSION%.zip .\bin\%RELEASEBUILD%\%FRAMEWORK%\%MACVERSION%\publish\*
+  7z a -tzip .\bin\%RELEASEBUILD%\%LINUXVERSION%.zip .\bin\%RELEASEBUILD%\%FRAMEWORK%\%LINUXVERSION%\publish\*
+  
+  pause
 endlocal
